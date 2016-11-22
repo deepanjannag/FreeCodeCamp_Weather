38ddc4173e89eff27621f543dc6d506a66f09ddd
Deepanjan Nag <subscriptions@deepanjannag.com>
2016-11-22 21:29:37 +0000

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -4,7 +4,7 @@ if (navigator.geolocation) {
         //document.querySelector("#myData").innerHTML = "latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude;
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
-        var url = 'http://api.openweathermap.org/data/2.5/weather?appid=129c9726ce601a0f49f47285d93387ba&lat=' + latitude + '&lon=' + longitude;
+        var url = 'https://api.openweathermap.org/data/2.5/weather?appid=129c9726ce601a0f49f47285d93387ba&lat=' + latitude + '&lon=' + longitude;
         $.getJSON(url, function (reply) {
             document.querySelector('#city').innerHTML = reply.name + ', ' + reply.sys.country;
             document.querySelector('#temp').innerHTML = '<span id="quotedTemperature">' + (reply.main.temp - 273).toFixed(1) + '</span>' + '<span id="degrees" onclick="clicked()"> °C</span>';
